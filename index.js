1d8ac07b52638fed0897f822b72a902dda5495ed
Denis Kibichiy <deniskibichiy@outlook.com>
2025-09-02 09:31:29 +0300

    add UI and set up the layout

--- index.js
+++ index.js
@@ -28,3 +28,45 @@ function operate (a, b, c) {
     return a(b,c);
 }
 
+for (let i = 0; i <= 20; i++) {
+    let button = document.createElement("button");
+    if( i % 5 == 0) {
+        button.style.cssText = "border: 0; height: 0; width: 100%";
+    }
+    if(i < 5 && i % 5 !== 0) {
+        button.innerText = i -1;
+    }
+    if (i > 5 && i < 10) {
+        button.innerHTML = i-2;
+    }
+    if(i > 10 && i < 15) {
+        button.innerHTML = i-3;
+    }
+    if(i > 12 && i < 14) {
+        button.innerHTML = i -4;
+    }
+
+    if(i ==13) {
+        button.innerHTML = "/"
+    } 
+    if (i == 14) {
+        button.innerHTML = "+"
+    }
+    if (i == 16) {
+        button.innerHTML = "-"
+    }
+    if (i == 17) {
+        button.innerHTML = "*"
+    }
+    if (i == 18) {
+        button.innerHTML = "="
+    }
+    if (i == 19) {
+        button.innerHTML = "Clear"
+    }
+    //if (i == 15) {
+        //button.innerHTML = "Clear"
+  //  }
+    
+    container.appendChild(button);
+}
